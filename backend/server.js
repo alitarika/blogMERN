c517040620f4aa77112083f1009c1a716426a516
alitarika <alitarikalacam@gmail.com>
2024-02-14 20:22:36 +0300

    some more init

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
-var a = Date.now();
+import express from "express";
 
-console.log(a);
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ mesaj: "hi from sw to postman" });
+});
+
+app.listen(3001, () => {
+  console.log("listening on 3001");
+});
