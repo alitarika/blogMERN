f10993cd6a4f71c6aa99e5a5410e1e45477119e2
alitarika <alitarikalacam@gmail.com>
2024-02-14 23:02:21 +0300

    router refactor. dotenv and mongoose init

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from "express";
+import { postsRoutes } from "./routes/postsRoutes.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const { PORT, MONGODBURL } = process.env;
 
 app.use(express.json());
+app.use("/", postsRoutes);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ mesaj: "hi from sw to postman" });
-});
-
-app.listen(3001, () => {
-  console.log("listening on 3001");
-});
+mongoose
+  .connect(MONGODBURL)
+  .then(() => {
+    console.log("Successfully connected to DB");
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
