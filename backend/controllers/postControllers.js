4a20e6025832d3b36a7cdd21d649c0ae8d9e2b73
alitarika <alitarikalacam@gmail.com>
2024-02-15 03:57:14 +0300

    Deleting post -> controller and route implemented.

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,6 +1,7 @@
+import mongoose from "mongoose";
 import Post from "../models/PostModel.js";
 
-// Get all blog posts.
+// Get *all* blog posts.
 export const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
@@ -10,7 +11,7 @@ export const getPosts = async (req, res) => {
   }
 };
 
-// Create new blog post.
+// Create a new blog post.
 export const addPost = async (req, res) => {
   // Parse data from request body
   const { title, body } = req.body;
@@ -26,6 +27,39 @@ export const addPost = async (req, res) => {
   try {
     const post = await Post.create({ title, body });
     res.status(200).json({ success: "Post is successfully created.", post });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete a blog post by ID.
+export const deletePost = async (req, res) => {
+  // Parse id from the url/request paramaters.
+  const { id } = req.params;
+
+  // Check whether or not ID is of valid type.
+  // If not return error.
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      error: "No blog post is found with this ID. (ID is invalid type.)",
+    });
+  }
+
+  // Check whether the post with that ID exists.
+  const post = await Post.findById(id);
+  if (!post) {
+    return res
+      .status(400)
+      .json({ error: "No blog post is found with this ID." });
+  }
+
+  // Parse title for verbose response. Delete post. Catch error if any.
+  try {
+    const { title } = post;
+    await post.deleteOne();
+    return res.status(200).json({
+      success: `Post with the title '${title}' is deleted.`,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
-import { getPosts, addPost } from "../controllers/postControllers.js";
+import {
+  getPosts,
+  addPost,
+  deletePost,
+} from "../controllers/postControllers.js";
 
 const router = express.Router();
 
 // Get all blog posts.
 router.get("/", getPosts);
 
-// Create new blog post.
+// Create a new blog post.
 router.post("/", addPost);
+
+// Delete a blog post.
+router.delete("/:id", deletePost);
 
 export { router as postsRoutes };
