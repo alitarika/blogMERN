0f5474aa3d29aca9afa64fb9710d6d858f7ce54c
alitarika <alitarikalacam@gmail.com>
2024-02-17 06:41:14 +0300

    update postschema to connect user from user_id (user objectid)

--- backend/models/PostModel.js
+++ backend/models/PostModel.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
   {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     title: {
       type: String,
       required: true,
