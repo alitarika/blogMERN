d10ebe40c63c05b27a5cfe81df73058825074326
alitarika <alitarikalacam@gmail.com>
2024-02-20 03:25:24 +0300

    posts controller crud

---
+++ frontend/src/pages/posts/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section className="card">
+      <h1 className="title">Latest Posts</h1>
+      <div>posts</div>
+    </section>
+  );
+};
+
+export default Home;
