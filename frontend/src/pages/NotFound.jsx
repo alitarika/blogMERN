e30bb0a2c2d6ccc3250fc191fe15f73be0ab7327
alitarika <alitarikalacam@gmail.com>
2024-02-21 18:46:17 +0300

    tone of red

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -7,8 +7,8 @@ const NotFound = () => {
         <i className="fa-solid fa-hand-point-left"></i>
         &nbsp; Back to homepage
       </Link>
-      <i className=" text p-2 mt-8 text-red-600 fa-solid fa-ban text-[100px] text-center block"></i>
-      <p className=" text p-2 mt-8 text-red-600  text-[50px] text-center block">
+      <i className=" text p-2 mt-8 text-red-500 fa-solid fa-ban text-[100px] text-center block"></i>
+      <p className=" text p-2 mt-8 text-red-500  text-[50px] text-center block">
         404 Not Found
       </p>
     </>
