01b361438d96b1e41cbc67bfc86145cd76351e39
alitarika <alitarikalacam@gmail.com>
2024-02-21 19:17:14 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { postsRoutes } from "./routes/postsRoutes.js";
 import { usersRoutes } from "./routes/usersRoutes.js";
 import mongoose from "mongoose";
@@ -9,7 +10,9 @@ dotenv.config();
 
 const { PORT, MONGODBURL } = process.env;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
 
